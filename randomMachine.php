30496b968f196598d52aca8fb9861ad1824c893d
Armoredbrain <l1ght3rth4nl1ght@gmail.com>
2021-01-08 11:11:33 +0100

    initial commit

---
+++ randomMachine.php
@@ -0,0 +1,7 @@
+<?php
+function randomMachine($n,$l)
+{
+    $bytes = openssl_random_pseudo_bytes($l);
+    $randomNumber = hexdec(bin2hex($bytes));
+    return ($randomNumber <= $n) ? $randomNumber:randomMachine($n,$l);
+}
